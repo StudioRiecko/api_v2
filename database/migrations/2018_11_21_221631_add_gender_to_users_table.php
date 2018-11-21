1f33ba9a37913ee649807ffd527d2359007b0b85
RIECKO\Riecko <riecko@gmail.com>
2018-11-21 23:33:10 +0100

    gender nullable

--- database/migrations/2018_11_21_221631_add_gender_to_users_table.php
+++ database/migrations/2018_11_21_221631_add_gender_to_users_table.php
@@ -14,7 +14,7 @@ class AddGenderToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function($table) {
-            $table->integer('gender')->after('name');
+            $table->integer('gender')->after('name')->nullable();
         });
     }
 
