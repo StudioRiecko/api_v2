434be21783c30957b69a806a96e0dbcdbf152912
RIECKO\Riecko <riecko@gmail.com>
2018-11-22 00:08:18 +0100

    int to string gender

--- database/migrations/2018_11_21_221631_add_gender_to_users_table.php
+++ database/migrations/2018_11_21_221631_add_gender_to_users_table.php
@@ -14,7 +14,7 @@ class AddGenderToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function($table) {
-            $table->integer('gender')->after('name')->nullable();
+            $table->string('gender')->after('name')->nullable();
         });
     }
 
